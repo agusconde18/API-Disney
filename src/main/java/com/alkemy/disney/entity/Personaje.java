adc8c37479a2f86574b173cd8e9fb367fbc6e62b
Agustin <agus.conde18@gmail.com>
2022-05-23 15:29:57 -0300

    Se agrega campos faltantes en la Interface de Personaje.java

--- src/main/java/com/alkemy/disney/entity/Personaje.java
+++ src/main/java/com/alkemy/disney/entity/Personaje.java
@@ -1,12 +1,15 @@
 package com.alkemy.disney.entity;
 
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 import javax.persistence.*;
 
 @Entity
 @Table(name = "personaje")
-@Data
+@Getter
+@Setter
 public class Personaje {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -16,6 +19,8 @@ public class Personaje {
     private String name;
 
     private Long age;
+
+    private String image;
 
     private Float weight;
 
