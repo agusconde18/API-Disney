5fff97edb415a0ec49bdec04ce413ada144c1de1
Julia Scodelari <j.scodelari96@gmail.com>
2022-05-23 14:19:29 -0300

    primer commit

--- src/main/java/com/alkemy/disney/DisneyApplication.java
+++ src/main/java/com/alkemy/disney/DisneyApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DisneyApplication {
 
+	//Agregando un comentarios inutil
+
 	public static void main(String[] args) {
 		SpringApplication.run(DisneyApplication.class, args);
 	}
