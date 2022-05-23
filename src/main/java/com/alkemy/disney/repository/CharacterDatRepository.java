330a1a7463d08f492c6196c0a27a111c707943f2
Agustin <agus.conde18@gmail.com>
2022-05-23 16:16:49 -0300

    Cambio de carpeta character dat Repository

--- src/main/java/com/alkemy/disney/repository/CharacterDatRepository.java
+++ src/main/java/com/alkemy/disney/repository/CharacterDatRepository.java
@@ -1,11 +1,12 @@
-package com.alkemy.disney.entity;
+package com.alkemy.disney.repository;
 
+import com.alkemy.disney.entity.CharacterDat;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 import java.util.Optional;
 
-public interface CharacterDatInterface extends JpaRepository<CharacterDat, Long> {
+public interface CharacterDatRepository extends JpaRepository<CharacterDat, Long> {
 
     @Override
     List<CharacterDat> findAll();
