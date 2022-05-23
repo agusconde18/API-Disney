ec06214c807a80a68f47d0d4c2622c99095b7eea
Agustin <agus.conde18@gmail.com>
2022-05-23 15:27:46 -0300

    Se agrega a la interface definiciones para usar luego

--- src/main/java/com/alkemy/disney/entity/PersonajeInterface.java
+++ src/main/java/com/alkemy/disney/entity/PersonajeInterface.java
@@ -3,11 +3,18 @@ package com.alkemy.disney.entity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface PersonajeInterface extends JpaRepository<Personaje, Long> {
 
     @Override
     List<Personaje> findAll();
+
+    @Override
+    void deleteById(Long id);
+
+    @Override
+    Optional<Personaje> findById(Long aLong);
 
     //... Agregar interfaces necesarias
 }
