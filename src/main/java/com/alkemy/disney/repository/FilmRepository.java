d7eb7eacd626163446ee3ee217e531470a0ea15e
Julia Scodelari <j.scodelari96@gmail.com>
2022-05-26 13:23:05 -0300

    borrado de carpetas

--- src/main/java/com/alkemy/disney/repository/FilmRepository.java
+++
@@ -1,9 +0,0 @@
-package com.alkemy.disney.repository;
-
-import com.alkemy.disney.entity.Film;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface FilmRepository extends JpaRepository<Film, Long> {
-}
