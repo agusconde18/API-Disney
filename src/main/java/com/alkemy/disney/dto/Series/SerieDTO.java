e4a02bf17e4c437cea96bfb1ab3db926e82b61af
BrianCiszewski-bbva <brianmatias.ciszewskialonso@bbva.com>
2022-05-31 12:46:10 -0300

    SerieDTO creado

--- src/main/java/com/alkemy/disney/dto/Series/SerieDTO.java
+++ src/main/java/com/alkemy/disney/dto/Series/SerieDTO.java
@@ -1,2 +1,28 @@
-package com.alkemy.disney.dto.Series;public class SerieDTO {
+package com.alkemy.disney.dto.Series;
+
+import com.alkemy.disney.entity.CharacterDat;
+import com.alkemy.disney.entity.Genre;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+@Getter
+@Setter
+public class SerieDTO {
+    private Long id;
+
+    private String title;
+    private Double rating;
+    private Date releaseDate;
+    private String coverImage;
+    private Integer totalEpisodes;
+
+    @JsonIgnoreProperties("actFilm")
+    Set<CharacterDat> characters = new HashSet<>();
+
+    private Genre genre;
 }
