d2ebb1183863eabf374b40584c122bbc70dd05fb
BrianCiszewski-bbva <brianmatias.ciszewskialonso@bbva.com>
2022-05-31 12:45:08 -0300

    Json ignore agregado para evitar recursividad

--- src/main/java/com/alkemy/disney/entity/Serie.java
+++ src/main/java/com/alkemy/disney/entity/Serie.java
@@ -1,5 +1,6 @@
 package com.alkemy.disney.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -25,6 +26,7 @@ public class Serie {
     private Integer totalEpisodes;
 
     @ManyToMany(fetch = FetchType.LAZY, cascade = CascadeType.PERSIST)
+    @JsonIgnoreProperties("actFilm")
     Set<CharacterDat> characters = new HashSet<>();
 
     @ManyToOne(fetch = FetchType.EAGER)
