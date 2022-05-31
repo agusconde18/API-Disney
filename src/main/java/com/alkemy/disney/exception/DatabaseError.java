5a372b2cf3f14a33b9f84e5d93fdc607b7546492
Agustin <agus.conde18@gmail.com>
2022-05-31 14:34:17 -0300

    Se soluciona DatabaseError

--- src/main/java/com/alkemy/disney/exception/DatabaseError.java
+++ src/main/java/com/alkemy/disney/exception/DatabaseError.java
@@ -2,6 +2,6 @@ package com.alkemy.disney.exception;
 
 public class DatabaseError extends Exception{
     public DatabaseError(String msn) {
-        super(ErrorMessages.DATABASE_ERROR);
+        super(msn);
     }
 }
