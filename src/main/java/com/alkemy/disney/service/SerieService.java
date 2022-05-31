04d81de7ade03ec0703cc18dad32ec44b648a7d6
BrianCiszewski-bbva <brianmatias.ciszewskialonso@bbva.com>
2022-05-31 13:12:17 -0300

    SerieService interfaz creada

---
+++ src/main/java/com/alkemy/disney/service/SerieService.java
@@ -0,0 +1,18 @@
+package com.alkemy.disney.service;
+
+import com.alkemy.disney.dto.Characters.PostCharactersDTO;
+import com.alkemy.disney.dto.Series.SerieDTO;
+import com.alkemy.disney.dto.Series.SerieListDTO;
+import com.alkemy.disney.dto.Series.SeriePostDTO;
+import com.alkemy.disney.entity.Serie;
+
+public interface SerieService {
+    SerieDTO save(SeriePostDTO serie);
+    void delete(Long id);
+    SerieDTO update(Long id);
+    SerieDTO updateCharacters(Long id, Long characterId);
+    SerieDto updateNewCharacter(Long id, PostCharactersDTO newChar);
+    SerieDTO deleteCharacter(Long id, Long characterId);
+    SerieDTO getSerieDetails(Long id);
+    List<SerieListDTO> getAllSeries();
+}
